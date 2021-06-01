8a49667a726dda5fbeef84a563199ad435aa839f
Chanapai Suparbpong <chanapaisuparbpong@Chanapais-MacBook-Pro.local>
2021-06-01 22:53:23 +0700

    [🐛]

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Worlddddd! NAJAAAATESTTTTdsadsaddasassa');
+  res.send('Hello Worlddddd! TESTNAKUB');
 });
 
 var server = app.listen(3000, function () {
